4a7213fdfc41eeff713da3bb8909eb9019a23394
GitSoyun <pinktenshi@naver.com>
2021-03-16 15:50:06 +0900

    BookVo 생성 [정소윤]

---
+++ spring_a/src/main/java/com/sist/goods/vo/BookVo.java
@@ -0,0 +1,18 @@
+package com.sist.goods.vo;
+
+import com.sist.goods.vo.BookVo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookVo {
+	private int no;
+	private String name;
+	private int price;
+	private String publisher;
+}
+
